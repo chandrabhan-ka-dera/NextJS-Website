140e787e34f601e4515daed4d62ff4dba63a6db6
Shiv Singh <shivsingh7150@hotmail.com>
2023-08-23 19:08:28 +0530

    cleen data

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 import Meta from '@components/Meta'
 import Header from '@components/Header'
-import Sidebar from '@components/Sidebar'
+
 import Footer from '@components/Footer'
 import Js from '@components/Js'
 
@@ -11,9 +11,8 @@ export default function Home() {
         <>
             <Meta />
             <Head>
-                <title>404 Page Not Found | Ashok Seeds and Plants</title>
+                <title>404 Page Not Found | Chandrabhan ka dera</title>
                 <meta name="description" content="" />
-
             </Head>
             <div className="page-wrapper">
                 <Header />
@@ -25,11 +24,11 @@ export default function Home() {
                             <div className="row">
                                 <div className="col-lg-8 offset-lg-2">
                                     <div className="error-content">
-                                        <img src="/img/404.png" alt="Iamge"/>
-                                            <h2>Oops! Page Not Found</h2>
-                                            <p>The page you are looking for might have been removed had its name changed
-                                                or is temporarily unavailable.</p>
-                                            <a href="https://www.ashokseedplant.com/" className="btn style1">Back To Home</a>
+                                        <img src="/img/404.png" alt="Iamge" />
+                                        <h2>Oops! Page Not Found</h2>
+                                        <p>The page you are looking for might have been removed had its name changed
+                                            or is temporarily unavailable.</p>
+                                        <a href="https://www.ashokseedplant.com/" className="btn style1">Back To Home</a>
                                     </div>
                                 </div>
                             </div>
